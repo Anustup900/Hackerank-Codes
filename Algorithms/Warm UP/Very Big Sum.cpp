0414a7197c084e845e71c68a95afdef65c4d8604
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-08-21 19:17:29 +0530

    Create Very Big Sum.cpp

---
+++ Algorithms/Warm UP/Very Big Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long i ,sum=0;
+    int n ;
+    cin>>n;
+    while(n--){
+        
+        cin >>i;
+        sum=sum+i;
+    }
+    cout<<sum<<endl;
+}
