0810c2c3667a15835d164d704c685d5f0f5b8533
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-08-21 19:34:52 +0530

    Update Diagonal Difference.cpp

--- Algorithms/Warm UP/Diagonal Difference.cpp
+++ Algorithms/Warm UP/Diagonal Difference.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-using namespace std ;
-int main()    {
-   int n ;
-   cin>> n ;
-   int a1,a2,a3,b1,b2,b3,c1,c2,c3;
-   cin>>a1>>a2>>a3;
-   cin>>b1>>b2>>b3;
-   cin>>c1>>c2>>c3;
-   int d,e,f;
-   d=(a1+b2+c3);
-   e=(a3+b2+c1);
-   f=abs(d-e);
-   cout<<f<<endl;
+using namespace std;
+int main() {
+    int n,ar[102][102],d1 = 0,d2 = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> ar[i][j];
+            if(i==j)d1+=ar[i][j];
+            if(i+j==n-1)d2+=ar[i][j];
+        }
+    }
+    cout << abs(d1-d2) << endl;
+    return 0;
 }
+
