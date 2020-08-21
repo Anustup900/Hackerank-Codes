2e6acc4c3e9d46dd04c3af8fd63aec730d84ff43
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2020-08-21 18:41:27 +0530

    Create Simple Array Sum.cpp

---
+++ Algorithms/Warm UP/Simple Array Sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+int main(){
+
+    int n , i , sum=0;
+    cin>>n ; 
+    while(n--){
+        cin >>i;
+        sum=sum+i;
+    }
+    cout <<sum <<endl;
+}
